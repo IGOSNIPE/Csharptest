87138ecccb7f1936575e044ff35fce35a36ce27d
Igor <Igorspychalaa@gmail.com>
2018-10-21 20:17:27 +0200

    Podstawa programu

---
+++ 1710/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _1710
+{
+    
+
+    class Program
+    {
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void WczytajLiczbyIWypisz()
+        {
+            int a = System.Int32.Parse(System.Console.ReadLine());
+            int b = System.Int32.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("{0} + {1} = {2}", a, b, Suma(a, b));
+        }
+
+        static void Main(string[] args)
+        {
+            //Powiedzmy że przyjeżdża do ciebie producent z fajnymi super fantastycznymi mebelkami, jest to krzeslo stol i szaf
+            // Powiedzmy ze do Twojego sklepu przyszedl ktos kupic szafke, krzeslo i stol a Ty musisz zebrac zamowienie
+            // Napisz program do zbierania takiego zamowienia i wyswietlania na ekran ceny calkowitej
+            System.Console.WriteLine("Co byś chcioł?\n1. krzyslo \n2.stol\n3.szefke\n4.wychodze stad");
+            int wybor = System.Int32.Parse(System.Console.ReadLine());
+            if(wybor != 4)
+            {
+                System.Console.WriteLine("Podaj producenta");
+                string producent = System.Console.ReadLine();
+                System.Console.WriteLine("Podaj kolekcje");
+                string kolekcja = System.Console.ReadLine();
+                System.Console.WriteLine("Podaj cene");
+                double cena = System.Double.Parse(System.Console.ReadLine());
+
+                if (wybor == 1)
+                {
+                    System.Console.WriteLine("Poda mnie obicie tygo krzysla:\n1. Drewno \n2. Welna\n3. Jedwab \n4.Diament");
+                    int obicieJakoInt = System.Int32.Parse(System.Console.ReadLine());
+                    krzeslo.Materialy obicie = (krzeslo.Materialy)obicieJakoInt;
+                    //krzeslo noweKrzeslo = new krzeslo(producent, ...);
+                    krzeslo noweKrzeslo = new krzeslo(producent, kolekcja, obicie, cena);
+                }
+                if (wybor == 2)
+                {
+
+                }
+            }
+            // tutaj wyjscie z programu czy tam wyswietlenie ceny
+        }
+    }
+}
---
+++ 1710/mebel.cs
@@ -0,0 +1,66 @@
+﻿/* 
+     * 1.60 Napisz klasę mebel zawierającą publiczne pola producent i kolekcja
+oraz klasy krzeslo, stol, szafka pochodne klasy mebel. Klasy pochodne
+powinny posiadac następujące pola publiczne:
+— klasa krzeslo pole obicie,
+— klasa stol pola szerokosc i dlugosc,
+— klasa szafka pola szerokosc, wysokosc i glebokosc
+*/
+namespace _1710
+{
+    class mebel
+    {
+        public string producent;
+        public string kolekcja;
+        public double cena;
+
+        protected mebel(string producent, string kolekcja, double cena)
+        {
+            this.producent = producent;
+            this.kolekcja = kolekcja;
+            this.cena = cena;
+        }
+    }
+
+    class krzeslo : mebel
+    {
+        public enum Materialy
+        {
+            Drewno = 1,
+            Welna,
+            Jedwab,
+            Diament
+        }
+
+        public Materialy obicie;
+
+        public krzeslo(string producent, string kolekcja, Materialy obicie, double cena) :
+            base(producent, kolekcja, cena)
+        {
+            this.obicie = obicie;
+        }
+    }
+    class stol : mebel
+    {
+        public double szerokosc, dlugosc;
+
+        public stol(string producent, string kolekcja, double szerokosc, double dlugosc, double cena) :
+            base(producent, kolekcja, cena)
+        {
+            this.szerokosc = szerokosc;
+            this.dlugosc = dlugosc;
+        }
+    }
+    class szafka : mebel
+    {
+        public double szerokosc, wysokosc, glebokosc;
+
+        public szafka(string producent, string kolekcja, double szerokosc, double wysokosc, double glebokosc, double cena) :
+            base(producent, kolekcja, cena)
+        {
+            this.szerokosc = szerokosc;
+            this.wysokosc = wysokosc;
+            this.glebokosc = glebokosc;
+        }
+    }
+}
